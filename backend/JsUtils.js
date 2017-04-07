427a0f0b5daf38a021cc9a1a4a2ad22e7ce3709c
kpaxico <ipek.guven@idesse.com.tr>
2017-04-07 16:25:15 +0300

    Min/Max Date controls are added to DatePicker Generic SelectionEntry control is added

--- backend/JsUtils.js
+++ backend/JsUtils.js
@@ -9,12 +9,25 @@ var isDate = function(v){
   return v && v instanceof Date;
 };
 
+var strToDate = function(dateStr, parseFormatStr, formatStr) {
+  return moment(dateStr, parseFormatStr).format(formatStr || 'DD/MM/YYYY');
+};
+
 var formatDate = function(dateVal, formatString) {
   return (isDate(dateVal) || moment.isMoment(dateVal)) ? moment(dateVal).format(formatString || 'DD/MM/YYYY') : '(invalid date value)';
+};
+
+String.prototype.format = function() {
+  var args = Array.prototype.slice.call(arguments, 0, arguments.length);
+  // debug_log('String.format.args: ' + args + ', this: ' + this);
+  return this.replace(/\{(\d+)\}/g, function(m, i) {
+    return args[i];
+  });
 };
 
 module.exports = {
   isEmpty,
   isDate,
+  strToDate,
   formatDate
 }
